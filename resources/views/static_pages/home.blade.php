c2fe3f3a575a1574ce4b5d67db0e92200a1ab1e4
tian.chuan <tian.chuan@immomo.com>
2018-02-11 14:36:10 +0800

    blade view

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Sample App</title>
-</head>
-<body>
-<h1>主页</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('title', '主页')
+@section('content')
+    <h1>帮助页</h1>
+@stop
