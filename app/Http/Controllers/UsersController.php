b9cbca40a1770f0dae7483e7cc1d0cb10dbee0fa
tianchuan <tianchzx@gmail.com>
2018-03-01 10:14:21 +0800

    update

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     public function create(Request $request){
-        trigger_error($request->url());
+        //trigger_error($request->url());
         return view('users.create');
     }
 }
